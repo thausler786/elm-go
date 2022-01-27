bccf127fd015c7c27ab915a0584804074a27c736
Tim Hausler <thausler786@gmail.com>
2022-01-26 16:11:02 -0800

    Renders the number of cells dynamically, given a position

--- src/Main.elm
+++ src/Main.elm
@@ -1,45 +1,67 @@
-module Main exposing (render, init)
+module Main exposing (init, render)
 
-import Html exposing (text, div)
+import Html exposing (div, text)
 import Html.Attributes exposing (style)
 
-grid = div [style "width" "360px"
-            , style "height" "360px"
-            , style "grid-template-columns" "repeat(9, 40px)"
-            , style "grid-template-rows" "repeat(9, 40px)"
-            , style "display" "grid"
-           ] gridElements
+
+grid =
+    div
+        [ style "width" "360px"
+        , style "height" "360px"
+        , style "grid-template-columns" "repeat(9, 40px)"
+        , style "grid-template-rows" "repeat(9, 40px)"
+        , style "display" "grid"
+        ]
+        gridElements
 
 
+gridElements =
+    [ intersectionDiv ]
 
-gridElements = [ intersection ]
 
+intersectionDiv =
+    div
+        [ style "width" "40px"
+        , style "height" "40px"
+        , style "background-color" "#FFC300"
+        , style "border" "dashed 1px black"
+        ]
+        []
 
-intersection = div [style "width" "40px"
-            , style "height" "40px"
-            , style "background-color" "#FFC300"
-            , style "border" "dashed 1px black"
-            ] []
 
 render : Position -> List (Html.Html msg)
-render position = gridElements
+render position =
+    position.board |> List.foldl (List.append) [] |> List.map renderIntersection
+
+renderIntersection intersectionRecord = intersectionDiv
 
 -- MAIN
 
-main = grid
+
+main =
+    grid
+
+
 
 -- MODEL
 
-type alias Intersection = 
-        { state : State
-        }
 
-type alias Position = 
-        { dimensions : Int
-        , board : List (List Intersection) }
+type alias Intersection =
+    { state : State
+    }
 
-type State = Black | White | Empty
+
+type alias Position =
+    { dimensions : Int
+    , board : List (List Intersection)
+    }
+
+type State
+    = Black
+    | White
+    | Empty
+
 
 init : Position
-init = { board =  [[{state = Empty}, {state = Black}]], dimensions = 9 }
-
+init =
+    { board = [ [ { state = Empty }, { state = Black } ] ], dimensions = 9 }
--- tests/Example.elm
+++ tests/Example.elm
@@ -2,23 +2,32 @@ module Example exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
-import Test exposing (..)
 import Html exposing (div)
 import Html.Attributes exposing (style)
+import Main exposing (init, render)
+import Test exposing (..)
 
-import Main exposing (render, init)
 
 suite : Test
 suite =
     describe "Rendering a board"
         [ test "renders a number of spaces equal to those in the model" <|
-            \_ -> 
+            \_ ->
                 render init
-                    |> Expect.equal [div [style "width" "40px"
-                                           , style "height" "40px"
-                                           , style "background-color" "#FFC300"
-                                           , style "border" "dashed 1px black"
-                                          ] []
-                                    ]
+                    |> Expect.equal
+                        [ div
+                            [ style "width" "40px"
+                            , style "height" "40px"
+                            , style "background-color" "#FFC300"
+                            , style "border" "dashed 1px black"
+                            ]
+                            []
+                        , div
+                            [ style "width" "40px"
+                            , style "height" "40px"
+                            , style "background-color" "#FFC300"
+                            , style "border" "dashed 1px black"
+                            ]
+                            []
+                        ]
         ]
-              
